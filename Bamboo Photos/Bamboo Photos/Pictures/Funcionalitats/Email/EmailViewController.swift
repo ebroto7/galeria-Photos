9752362904b51addb0f0bd29d6964e88ce964a32
enric <enrique7siete@gmail.com>
2021-02-22 17:31:15 +0100

    update - enviar correu

--- Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/Email/EmailViewController.swift
+++ Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/Email/EmailViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import MessageUI
 
 class EmailViewController: UIViewController {
 
@@ -25,16 +26,27 @@ class EmailViewController: UIViewController {
     
     
     @IBAction func botonEnviar(_ sender: Any) {
+        enviarMail()
     }
     
     @IBAction func botonGuardar(_ sender: Any) {
+        let popUpuserName = nameTextfield.text!
+        let popUpuserEmail = emailTextfield.text!
+        
+        if popUpuserName.isEmpty || popUpuserEmail.isEmpty {
+            let alert = UIAlertController(title: "Ja has escrit el teu nom i e-mail?", message: "Falten dades", preferredStyle: .actionSheet)
+
+                        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+                        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+            
+                        self.present(alert, animated: true)
+        } else {
+        
         saveToUserDefaults()
         updateTextview()
+        }
     }
     
-    func readName() {
-        
-    }
 
 
 // guardem la info al disc dur
@@ -57,7 +69,22 @@ class EmailViewController: UIViewController {
      
     }
     
-  
+// per enviar un mail al correu que introduim als textfiels
+    func enviarMail() {
+        let userName: String = nameTextfield.text!
+        let userEmail = emailTextfield.text!
+        
+        if MFMailComposeViewController.canSendMail() {
+            let mail = MFMailComposeViewController()
+            mail.mailComposeDelegate = self
+            mail.setToRecipients([userEmail])
+            mail.setMessageBody("Hola \(userName), \n lwugfuwhfiwenhcbfqwueh", isHTML: false)
+            
+            present(mail, animated: true)
+        }
+    }
+   
+    
 }
 
 extension EmailViewController: UITextFieldDelegate {
@@ -74,5 +101,11 @@ extension EmailViewController: UITextFieldDelegate {
         }
         
         return true
+    }
+}
+
+extension EmailViewController: MFMailComposeViewControllerDelegate {
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        controller.dismiss(animated: true)
     }
 }
