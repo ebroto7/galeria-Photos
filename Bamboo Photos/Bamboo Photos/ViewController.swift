a223f5fed470dcf0c5e5b74f0fd7a2512955aef4
enric <enrique7siete@gmail.com>
2021-02-22 09:57:30 +0100

    Generem el projecte Bamboo photos - Commit inicial

---
+++ Bamboo Photos/Bamboo Photos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bamboo Photos
+//
+//  Created by Enric Broto Hernandez on 22/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
