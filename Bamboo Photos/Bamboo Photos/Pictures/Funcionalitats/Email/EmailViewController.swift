be78c28cbae05de7fa3d62db72e0fb1b46c84e14
enric <enrique7siete@gmail.com>
2021-02-22 13:57:52 +0100

    comencem a introduir codi

--- Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/Email/EmailViewController.swift
+++ Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/Email/EmailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  Email_ViewController.swift
+//  EmailViewController.swift
 //  Bamboo Photos
 //
 //  Created by Enric Broto Hernandez on 22/2/21.
@@ -7,23 +7,45 @@
 
 import UIKit
 
-class Email_ViewController: UIViewController {
+class EmailViewController: UIViewController {
 
+    @IBOutlet weak var NameTextfield: UITextField!
+
+
+    
+    
+    @IBOutlet weak var emailTextfield: UITextField!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    func testSave() {
+        let userName: String = NameTextfield.text!
+        UserDefaults.standard.setValue(userName, forKey: "name")
+        UserDefaults.standard.synchronize()
     }
-    */
+    
+    
+}
 
+extension EmailViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("has clicat return")
+        if textField == NameTextfield {
+            print("has introduit \(NameTextfield.text)")
+            emailTextfield.becomeFirstResponder()
+        } else if  textField == emailTextfield{
+            print("has introdiut el \(emailTextfield.text)")
+            emailTextfield.resignFirstResponder()
+        } else {
+            print("no se que has clicat")
+        }
+        
+        return true
+    }
 }
