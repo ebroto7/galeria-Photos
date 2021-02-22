5918265238e9aebb45dd7648820ca8a1aa39a445
enric <enrique7siete@gmail.com>
2021-02-22 10:37:57 +0100

    tabBar creat

---
+++ Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/Email/EmailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  Email_ViewController.swift
+//  Bamboo Photos
+//
+//  Created by Enric Broto Hernandez on 22/2/21.
+//
+
+import UIKit
+
+class Email_ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
