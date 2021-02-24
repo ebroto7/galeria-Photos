f592fb6666b9476742db1f670ade0457a6b3283e
enric <enrique7siete@gmail.com>
2021-02-24 15:44:59 +0100

    treballem component switch de la pantalla picturesFullScreenView

--- Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/picturesCollection/PicturesFullScreenViewController.swift
+++ Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/picturesCollection/PicturesFullScreenViewController.swift
@@ -16,9 +16,25 @@ class PicturesFullScreenViewController: UIViewController {
     @IBOutlet weak var likeSwitch: UISwitch!
     
     
+    @IBAction func closeButton(_ sender: Any) {
+    print("sortim de la pantalla")
+    }
+    
+    
+    @IBAction func switchAction(_ sender: Any) {
+        if likeSwitch.isOn {
+        print("t'ha agradat la imatge")
+        } else {
+            print("ja no t'agrada")
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        likeSwitch.isOn = false
+        
     }
     
 
