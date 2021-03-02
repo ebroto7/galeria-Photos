de1e1e11554f13f29596f24cd035abc2f02b3553
enric <enrique7siete@gmail.com>
2021-03-02 14:10:17 +0100

    generem tomporitzador per la pantalla random

--- Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/RandomPicture/RandomPictureViewController.swift
+++ Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/RandomPicture/RandomPictureViewController.swift
@@ -6,28 +6,52 @@
 //
 
 import UIKit
+import Foundation
 
 class RandomPictureViewController: UIViewController {
 
     @IBOutlet weak var viewImage: UIImageView!
+    var timer: Timer?
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-   //     Int.random(in: 0...imageData.numOfImages())       //generem un número aleatori en 0 i el mnumero de imatges que tenim
-  
-//  fem que al entrar a la pantalla ens retorni una imatge aleatoria
-//        let image = imageData.imageForPosition(Int.random(in: 0...imageData.numOfImages()))
-//        viewImage.image = image
-        
-        
+        viewImage.image = getRandomPicture()
+      
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        viewImage.image = getRandomPicture()
+        
+        //     Int.random(in: 0...imageData.numOfImages())       //generem un número aleatori en 0 i el mnumero de imatges que tenim
+       
+     //  fem que al entrar a la pantalla ens retorni una imatge aleatoria
+     //        let image = imageData.imageForPosition(Int.random(in: 0...imageData.numOfImages()))
+     //        viewImage.image = image
+             
+           
+        
+//        viewImage.image = getRandomPicture()    // ho posem al viewDidAppear perque volem que cada com que aparegui la pantalla, carregui un la imatge diferent
+        
+//        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(sayHello), userInfo: nil, repeats: true)        //  creem i activem el temporitzador un cop hem entrat a la pantalla, SISTEMA ANTIC AMB Target I #Selector
+        
+        // el mateix timer d'abans escrit de manera mes "moderna"
+        timer = Timer.scheduledTimer(withTimeInterval: 3.0,
+                                     repeats: true,
+                                     block: { _ in
+                                        self.sayHello()
+        })
     }
     
+    @objc func sayHello() {
+        viewImage.image = getRandomPicture()
+//        timer?.invalidate()         // per aturar el timer
+        print("hello")
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        timer?.invalidate()         //aturem el temporitzador quan hem sortit de la pantalla
+    }
     
     func getRandomPicture() -> UIImage? {
         let minPictureIndex: Int = 0
