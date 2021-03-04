35d1481c9511a49744bf8caafc4514f757d673e5
enric <enrique7siete@gmail.com>
2021-03-04 12:35:14 +0100

    pujem nous canvis per a probes projecte git

--- Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/RandomPicture/RandomPictureViewController.swift
+++ Bamboo Photos/Bamboo Photos/Pictures/Funcionalitats/RandomPicture/RandomPictureViewController.swift
@@ -47,7 +47,7 @@ class RandomPictureViewController: UIViewController {
     
     @objc func sayHello() {
         UIView.transition(with: viewImage,
-                          duration: 1.5,
+                          duration: 100.5,
                           options: [.transitionCrossDissolve],
                           animations: {
                             let randomNumber = self.getRandomPictureIndex()
@@ -74,7 +74,9 @@ class RandomPictureViewController: UIViewController {
 //    }
     
     func getRandomPictureIndex() -> Int {
+        print("el meu projecte")
         let randomIndex = Int.random(in: 0...ImageData.numOfImages()-1)
+        
         return randomIndex
     }
     
